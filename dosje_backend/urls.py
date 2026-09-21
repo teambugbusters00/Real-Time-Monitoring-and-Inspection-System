7a673d63abbd3f0eebf0eb006eae81e61e6f69fd
vijay jangid <186843345+teambugbusters00@users.noreply.github.com>
2026-09-22 04:51:23 +0530

    Add Google authentication endpoint

--- dosje_backend/urls.py
+++ dosje_backend/urls.py
@@ -41,7 +41,7 @@ from django.views.generic import TemplateView, RedirectView
 
 from accounts.views import CustomTokenObtainPairView, DivisionViewSet, NGOViewSet, UserViewSet, NGOLoginView
 
-from accounts.views import LogoutView
+from accounts.views import LogoutView, GoogleLoginView
 from django.conf import settings
 from django.conf.urls.static import static
 from projects.views import PublicProjectListView
@@ -67,6 +67,7 @@ urlpatterns = [
     path('api/auth/login-ngo/', NGOLoginView.as_view(), name='login_ngo'),
     path('api/auth/login-beneficiary/', BeneficiaryLoginView.as_view(), name='login_beneficiary'),
     path('api/auth/logout/', LogoutView.as_view(), name='logout'),
+    path('api/auth/google/', GoogleLoginView.as_view(), name='google_login'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/beneficiary/me/', BeneficiaryMeView.as_view(), name='beneficiary_me'),
     path('api/beneficiary/complaints/', BeneficiaryComplaintView.as_view(), name='beneficiary_complaint'),
